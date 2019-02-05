b2995d85b1dfd289b2b98814fad87fe85abc2623
yogesh.linganna <yogesh.linganna@gmail.com>
2019-02-05 15:56:30 -0500

    AuthService and AngularFire integration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+//third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+//shared modules
+import { SharedModule } from './shared/shared.module';
+
 export const ROUTES: Routes = [
  {
    path:'auth',
@@ -13,10 +21,23 @@ export const ROUTES: Routes = [
  }
 ];
 
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: "XXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXX"
+};
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    SharedModule.forRoot()
   ]
 })
 export class AuthModule {}
--- src/auth/login/containers/login/login.component.ts
+++ src/auth/login/containers/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'login',
@@ -10,14 +13,30 @@ import { FormGroup } from '@angular/forms';
         <h1>Login</h1>
         <a routerLink="/auth/register">Not registered?</a>
         <button type="submit"> Login </button>
+        <div class="error" *ngIf="error">
+        {{ error }}
+        </div>
       </auth-form>
     </div>
   `
 })
 export class LoginComponent {
-  constructor() {}
 
-  loginUser(event: FormGroup){
-    console.log(event.value);
+  error: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  async loginUser(event: FormGroup) {
+    const { email, password } = event.value;
+    try {
+      await this.authService.loginUser(email, password);
+      this.router.navigate(['/']);
+    } catch (err) {
+      this.error = err.message;
+    }
+
   }
 }
